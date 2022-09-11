585e53707e0ccc222fb65d23c658831f306de410
Saurabh <srbhkumar594@gmail.com>
2022-09-11 20:56:28 +0530

    with internet gateway and rout table

--- main.tf
+++ main.tf
@@ -5,17 +5,16 @@
 provider "aws" {
 }
 
+variable "env" {}
 variable "vpc_cidr" {}
-variable "vpc_name" {}
 variable "subnet_cidr" {}
-variable "subnet_name" {}
 variable "avail_zone" {}
 
 
 resource "aws_vpc" "dev-vpc" {
   cidr_block = var.vpc_cidr
   tags = {
-    Name = "${var.vpc_name}_vpc"
+    Name = "${var.env}_vpc"
   }
 }
 
@@ -24,7 +23,35 @@ resource "aws_subnet" "dev-subnet-1" {
   cidr_block = var.subnet_cidr          # "192.168.20.0/24"
   availability_zone = var.avail_zone        # "ap-south-1a"
   tags = {
-    Name = "${var.subnet_name}_subnet"
+    Name = "${var.env}_subnet"
   }
 }
 
+# create aws internet gateway
+
+resource "aws_internet_gateway" "my_vpc_gateway" {
+  vpc_id = aws_vpc.dev-vpc.id
+  tags = {
+    "Name" = "${var.env}_gateway"
+  }
+}
+
+# create aws route table for dev_vpc
+
+resource "aws_route_table" "vpc_route_table" {
+  vpc_id = aws_vpc.dev-vpc.id
+  route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_vpc_gateway.id
+  } 
+  tags = {
+    "Name" = "${var.env}_rtb"
+  }
+}
+
+# connect route table with subnet 
+
+resource "aws_route_table_association" "associate_route_table" {
+  subnet_id = aws.subnet.dev-subnet-1.id
+  route_table_id = aws_route_table.vpc_route_table.id
+}
