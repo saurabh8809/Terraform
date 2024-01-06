61866351d109aea88ac60af6392d95e0ee72d66e
Saurabh Kumar <saurabh.kumar@soroco.com>
2024-01-06 10:48:08 +0530

    updated the code

--- main.tf
+++ main.tf
@@ -1,124 +1,117 @@
-# # printing output of resource value after resource creation 
-# # using output module 
-# from line 30 to 42 way to access default VPC
-
-provider "aws" {
-}
-
-variable "env" {}
-variable "vpc_cidr" {}
-variable "subnet_cidr" {}
-variable "avail_zone" {}
-variable "ami" {}
-variable "ec2_type" {}
-
-
-resource "aws_vpc" "dev-vpc" {
-  cidr_block = var.vpc_cidr
-  tags = {
-    Name = "${var.env}_vpc"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.74.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "terraform-state"
+    storage_account_name = "terraformstate321"
+    container_name       = "tfstate"
+    key                  = "test/terraform.tfstate"
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id = aws_vpc.dev-vpc.id
-  cidr_block = var.subnet_cidr          # "192.168.20.0/24"
-  availability_zone = var.avail_zone        # "ap-south-1a"
-  tags = {
-    Name = "${var.env}_subnet"
-  }
+provider "azurerm" { 
+  features {}
 }
 
-# create aws internet gateway
-
-resource "aws_internet_gateway" "my_vpc_gateway" {
-  vpc_id = aws_vpc.dev-vpc.id
-  tags = {
-    "Name" = "${var.env}_gateway"
-  }
+resource "azurerm_resource_group" "main-create-rg" {
+  location = var.location
+  name = var.rg-name
 }
 
-# create aws route table for dev_vpc
-
-resource "aws_route_table" "vpc_route_table" {
-  vpc_id = aws_vpc.dev-vpc.id
-  route  {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_vpc_gateway.id
-  } 
-  tags = {
-    "Name" = "${var.env}_rtb"
-  }
+resource "azurerm_virtual_network" "this" {
+  name = var.vnet-name
+  resource_group_name = azurerm_resource_group.main-create-rg.name
+  location = azurerm_resource_group.main-create-rg.location
+  address_space = ["192.168.1.0/24"]
 }
 
-# connect route table with subnet 
-
-resource "aws_route_table_association" "associate_route_table" {
-  subnet_id = aws_subnet.dev-subnet-1.id
-  route_table_id = aws_route_table.vpc_route_table.id
-}
-
-# Security group for ssh and nginx
-
-resource "aws_security_group" "nsg_ssh_nginx" {
-  name = "SSH_NGINX"
-  vpc_id = aws_vpc.dev-vpc.id
-
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    prefix_list_ids = []
-
-  }
-
-  tags = {
-    "Name" = "${var.env}_sg"
-  }
-}
-
-resource "aws_instance" "my-app" {
-  ami = var.ami
-  instance_type = var.ec2_type
-
-  subnet_id = aws_subnet.dev-subnet-1.id
-  security_groups = [aws_security_group.nsg_ssh_nginx.id]
-  availability_zone = var.avail_zone
-
-  associate_public_ip_address = true      # for assigning public ip to ec2 machine
-  key_name = "mumbai_key"
-
-  # user_data = <<EOF
-  #             #!/bin/bash
-  #             sudo yum update -y && sudo yum install docker -y
-  #             sudo systemctl start docker 
-  #             sudo usermod -aG docker ec2-user
-  #             sudo docker run -itd -p 8080:80 --name web-app nginx
-  # EOF
-  user_data = "${file("ec2-user-data.sh")}"
-  tags = {
-    Name = "${var.env}-server"
-  }
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.my-app.public_ip
+# resource "azurerm_subnet" "this" {
+#   name = "subnet1"
+#   resource_group_name = azurerm_resource_group.terraform-rg.name
+#   virtual_network_name = azurerm_virtual_network.this.name
+#   address_prefixes = ["192.168.1.0/25"]
   
-}
+# }
 
+# resource "azurerm_subnet" "main" {
+#   name = "subnet2"
+#   resource_group_name = azurerm_resource_group.terraform-rg.name
+#   virtual_network_name = azurerm_virtual_network.this.name
+#   address_prefixes = ["192.168.1.128/25"]
+  
+# }
 
+# resource "azurerm_public_ip" "this" {
+#   name = "terraform-vm-public-ip"  
+#   location = azurerm_resource_group.terraform-rg.location
+#   allocation_method = "Static"
+#   resource_group_name = azurerm_resource_group.terraform-rg.name
+# }
+
+# resource "azurerm_network_interface" "this" {
+#   name = "terraform-nic"
+#   resource_group_name = azurerm_resource_group.terraform-rg.name
+#   location = azurerm_resource_group.terraform-rg.location
+#   ip_configuration {
+#     name = "terraform-ip_configuration"
+#     subnet_id = azurerm_subnet.main.id
+#     private_ip_address_allocation = "Dynamic"
+#     # public_ip_address_id = azurerm_public_ip.this.ip_address
+
+#   }
+# }
+
+# resource "azurerm_network_security_group" "this" {
+#   name = "terrafrom-vm-nsg"
+#   resource_group_name = azurerm_resource_group.terraform-rg.name
+#   location = azurerm_resource_group.terraform-rg.location
+# }
+
+# resource "azurerm_network_security_rule" "this" {
+#   resource_group_name = azurerm_resource_group.terraform-rg.name
+#   network_security_group_name = azurerm_network_security_group.this.name
+#   name                        = "allow-ssh"
+#   priority                    = 100
+#   direction                   = "Outbound"
+#   access                      = "Allow"
+#   protocol                    = "Tcp"
+#   source_port_range           = "*"
+#   destination_port_range      = "22"
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "*"
+  
+# }
+
+# resource "azurerm_linux_virtual_machine" "this" {
+#   name = "terraform-vm"
+#   resource_group_name = azurerm_resource_group.terraform-rg.name
+#   location = azurerm_resource_group.terraform-rg.location
+#   size = "Standard_A1_v2"
+#   admin_username = "deb"
+#   admin_ssh_key {
+#     username = "deb"
+#     public_key = file("~/.ssh/id_rsa.pub")
+#   }
+
+#   network_interface_ids = [ azurerm_network_interface.this.id ]
+#   # network_interface_ids = azurerm_network_interface.this.id
+#   os_disk {
+#     caching = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer = "0001-com-ubuntu-server-jammy"
+#     sku = "22_04-lts"
+#     version = "latest"
+#   }
+
+# }
+
+# output "public_ip_address" {
+#   value = azurerm_public_ip.this.ip_address
+# }
---
+++ variable.tf
@@ -0,0 +1,3 @@
+variable "rg-name" { }
+variable "vnet-name" { }
+variable "location" { }
